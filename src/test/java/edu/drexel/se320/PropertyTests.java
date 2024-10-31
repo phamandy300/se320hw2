034e54e5d04056f8f39e1d5b8d7a94267ded54dc
Andy <ap3879@drexel.edu>
2024-10-30 23:51:18 -0400

    implemented unsorted, sorted, sorted+element array generators and some tests

--- src/test/java/edu/drexel/se320/PropertyTests.java
+++ src/test/java/edu/drexel/se320/PropertyTests.java
@@ -17,15 +17,68 @@ import net.jqwik.api.Tuple.Tuple2;
 import net.jqwik.api.constraints.*;
 import net.jqwik.api.statistics.Statistics;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Random;
+
 
 public class PropertyTests extends BinarySearchBase {
 
     @Property
     public boolean checkSingleArrayElt(@ForAll int x) {
-	    Integer[] arr = { x };
-	    return find(arr, x) == 0;
+        Integer[] arr = { x };
+        return find(arr, x) == 0;
     }
 
+    @Provide
+    Arbitrary<Integer[]> randomArr() {
+        Arbitrary<Integer> arbInt = Arbitraries.integers().between(0, 100);
+        return arbInt.array(Integer[].class).ofMinSize(1).ofMaxSize(30);
+    }
+
+    @Provide
+    Arbitrary<Integer[]> randomSortedArr() {
+        Arbitrary<Integer> arbInt = Arbitraries.integers().between(0, 100);
+        return arbInt.array(Integer[].class).ofMinSize(1).ofMaxSize(30).map(arr -> {
+            Arrays.sort(arr); return arr;});
+    }
+
+    @Provide
+    Arbitrary<Tuple2<Integer[], Integer>> arrayAndInt() {
+        Arbitrary<Integer> arbInt = Arbitraries.integers().between(0, 100);
+        return arbInt.array(Integer[].class)
+                .ofMinSize(1)
+                .ofMaxSize(30)
+                .map(arrInt -> {
+                    Arrays.sort(arrInt);
+                    int randomIndex = new Random().nextInt(arrInt.length);
+                    return Tuple.of(arrInt, arrInt[randomIndex]);
+                });
+    }
+
+    @Property
+    public boolean checkRandomSortedArray(@ForAll("arrayAndInt") Tuple2<Integer[], Integer> arrAndInt) {
+        Integer[] arr = arrAndInt.get1();
+        Integer element = arrAndInt.get2();
+
+        return Objects.equals(arr[find(arr, element)], element);
+    }
+
+    @Property
+    public void checkElementNotInArrayLowerBound(@ForAll("randomSortedArr") Integer[] arr) {
+        assertThrows(NoSuchElementException.class, () -> find(arr, -1));
+    }
+
+    @Property
+    public void checkElementNotInArrayUpperBound(@ForAll("randomSortedArr") Integer[] arr) {
+        assertThrows(NoSuchElementException.class, () -> find(arr, 101));
+    }
+
+    @Property
+    public void checkUnsortedArray(@ForAll("randomArr") Integer[] arr) {
+
+    }
 
 }
 
