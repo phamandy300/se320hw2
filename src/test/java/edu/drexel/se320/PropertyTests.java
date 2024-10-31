f8955ad7976e909c49505c237bf4a072e963ac11
Andy <ap3879@drexel.edu>
2024-10-31 14:42:45 -0400

    finished tests

--- src/test/java/edu/drexel/se320/PropertyTests.java
+++ src/test/java/edu/drexel/se320/PropertyTests.java
@@ -5,10 +5,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.lessThan;
+import static org.junit.jupiter.api.Assertions.*;
 
 // Core JUnit 5
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 // Jqwik
@@ -17,10 +16,7 @@ import net.jqwik.api.Tuple.Tuple2;
 import net.jqwik.api.constraints.*;
 import net.jqwik.api.statistics.Statistics;
 
-import java.util.Arrays;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 
 public class PropertyTests extends BinarySearchBase {
@@ -39,26 +35,22 @@ public class PropertyTests extends BinarySearchBase {
 
     @Provide
     Arbitrary<Integer[]> randomSortedArr() {
-        Arbitrary<Integer> arbInt = Arbitraries.integers().between(0, 100);
-        return arbInt.array(Integer[].class).ofMinSize(1).ofMaxSize(30).map(arr -> {
+        Arbitrary<Integer[]> randomArbArr = randomArr();
+        return randomArbArr.map(arr -> {
             Arrays.sort(arr); return arr;});
     }
 
     @Provide
     Arbitrary<Tuple2<Integer[], Integer>> arrayAndInt() {
-        Arbitrary<Integer> arbInt = Arbitraries.integers().between(0, 100);
-        return arbInt.array(Integer[].class)
-                .ofMinSize(1)
-                .ofMaxSize(30)
-                .map(arrInt -> {
-                    Arrays.sort(arrInt);
-                    int randomIndex = new Random().nextInt(arrInt.length);
-                    return Tuple.of(arrInt, arrInt[randomIndex]);
-                });
+        Arbitrary<Integer[]> sortedArbArr = randomSortedArr();
+        return sortedArbArr.map(arrInt -> {
+            int randomIndex = new Random().nextInt(arrInt.length);
+            return Tuple.of(arrInt, arrInt[randomIndex]);
+        });
     }
 
     @Property
-    public boolean checkRandomSortedArray(@ForAll("arrayAndInt") Tuple2<Integer[], Integer> arrAndInt) {
+    public boolean findElementInRandomSortedArray(@ForAll("arrayAndInt") Tuple2<Integer[], Integer> arrAndInt) {
         Integer[] arr = arrAndInt.get1();
         Integer element = arrAndInt.get2();
 
@@ -66,18 +58,67 @@ public class PropertyTests extends BinarySearchBase {
     }
 
     @Property
-    public void checkElementNotInArrayLowerBound(@ForAll("randomSortedArr") Integer[] arr) {
+    public void throwsForElementBelowLowerBound(@ForAll("randomSortedArr") Integer[] arr) {
         assertThrows(NoSuchElementException.class, () -> find(arr, -1));
     }
 
     @Property
-    public void checkElementNotInArrayUpperBound(@ForAll("randomSortedArr") Integer[] arr) {
+    public void throwsForElementAboveUpperBound(@ForAll("randomSortedArr") Integer[] arr) {
         assertThrows(NoSuchElementException.class, () -> find(arr, 101));
+    }
+    
+    @Example
+    public void throwsForEmptyArray() {
+        Integer[] arr = {};
+        assertThrows(IllegalArgumentException.class, () -> find(arr, 0));
+    }
+
+    @Example
+    public void throwsForNullArray() {
+        assertThrows(IllegalArgumentException.class, () -> find(null, 0));
     }
 
     @Property
-    public void checkUnsortedArray(@ForAll("randomArr") Integer[] arr) {
+    public void throwsForNullElement(@ForAll("randomSortedArr") Integer[] arr) {
+        assertThrows(IllegalArgumentException.class, () -> find(arr, null));
+    }
 
+    @Property
+    public void findFirstElement(@ForAll("randomSortedArr") @UniqueElements Integer[] arr) {
+        assertEquals(0, find(arr, arr[0]));
+    }
+
+    @Property
+    public void findLastElement(@ForAll("randomSortedArr") @UniqueElements Integer[] arr) {
+        assertEquals(arr.length - 1, find(arr, arr[arr.length - 1]));
+    }
+
+    @Property
+    public void findIntermediateElements(@ForAll("randomSortedArr") @UniqueElements Integer[] arr) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            int index = find(arr, arr[i]);
+            assertEquals(i, index);
+        }
+    }
+
+    @Property
+    public void findWithUnsortedArray(@ForAll("randomArr") Integer[] arr) {
+        try {
+            int elementToFind = arr[arr.length / 2];
+            int result = find(arr, elementToFind);
+            assertTrue(result >= 0 && result < arr.length);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException || e instanceof NoSuchElementException);
+        }
+    }
+
+    @Property
+    public void findDuplicateElements(@ForAll("randomSortedArr") Integer[] arr) {
+        for (Integer integer : arr) {
+            int index = find(arr, integer);
+            assertTrue(index >= 0 && index < arr.length);
+            assertEquals(integer, arr[index]);
+        }
     }
 
 }
